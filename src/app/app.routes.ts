918cfc6fd956e48790ca0bab08a8a5dba0d0bccc
pimientoyolo <mgefrad@gmail.com>
2026-01-27 14:05:39 -0500

    feat(routing): se enruta con cada pulsacion a componente correcto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,21 @@
 import { Routes } from '@angular/router';
 import { MainLayout } from './core/layout/main-layout/main-layout';
 import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Experience } from './pages/experience/experience';
+import { Skills } from './pages/skills/skills';
+import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayout,
     children: [
-      { path: '', component: Home }
-    ]
-  }
+      { path: '', component: Home },
+      { path: 'about', component: About },
+      { path: 'experience', component: Experience },
+      { path: 'skills', component: Skills },
+      { path: 'contact', component: Contact },
+    ],
+  },
 ];
