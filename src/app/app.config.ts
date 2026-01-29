9b051c01cca6e7d83dcf06f0839b21c5fda2008b
pimientoyolo <mgefrad@gmail.com>
2026-01-29 17:51:24 -0500

    feat(reverse)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
-/*Agregar el withHashLocation para hacer uso de rutas con referencia, para que funcione en github pages*/
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withHashLocation()),
+    provideRouter(routes),
     provideClientHydration(withEventReplay())
   ]
 };
